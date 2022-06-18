f4072b71f62f397f95d4b4d486962954d6bb8727
flyStar2008 <gcrocky2@163.com>
2022-06-18 14:45:45 +0800

    feat: add Flod component

---
+++ src/components/Fold.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div class="list-box" @click="unfoldHandle" :style="{'margin-top': marginTop}">
+    <span>{{label}}</span>
+    <el-icon>
+      <ArrowRightBold v-if="!showMore" />
+      <ArrowDownBold v-else />
+    </el-icon>
+  </div>
+   <transition name="fade">
+      <div v-if="showMore">
+        <slot></slot>
+      </div>
+   </transition>
+</template>
+<script setup>
+import { ref } from "vue";
+import {
+  ArrowRightBold,
+  ArrowDownBold
+} from '@element-plus/icons-vue'
+defineProps({
+  label: String,
+  marginTop: {
+    type: String,
+    default: '1px'
+  }
+})
+
+let showMore = ref(false)
+function unfoldHandle() {
+  showMore.value = !showMore.value
+}
+</script>
+
+<style scoped lang="scss">
+@mixin baseStyle($marginTop) {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  box-sizing: border-box;
+  margin-top: $marginTop;
+  height: 50px;
+  align-items: center;
+  background-color: white;
+  cursor: pointer;
+}
+
+.list-box {
+   @include baseStyle($marginTop: 50px);
+}
+</style>
